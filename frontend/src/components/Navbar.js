1e5d47dfd0816038523184a639e2be22c9b37b7a
impanaj07 <impanajgowda14@gmail.com>
2024-09-05 20:09:02 +0530

    Added chart component for emission analysis

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { IoLogOutOutline } from "react-icons/io5/index";
+import { IoLogOutOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 import { useFirebase } from '../context/Firebase';
 
