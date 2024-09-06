5a937a4a40150912c18152d196752c9ce1886545
Geethika-Kancharla <geethikak004@gmail.com>
2024-09-06 22:19:10 +0530

    Home page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,28 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
 
-
-
         <footer className="bg-black rounded-lg shadow m-4 dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between bg-gray-800">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    © 2024 CarbMine. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
-                        <a href="#" className="hover:underline mr-4 md:mr-6">About</a>
+                        <Link to="/" className="hover:underline mr-4 md:mr-6">Home</Link>
                     </li>
                     <li>
-                        <a href="#" className="hover:underline mr-4 md:mr-6">Privacy Policy</a>
+                        <Link to="/calculations" className="hover:underline mr-4 md:mr-6">Calculations</Link>
                     </li>
                     <li>
-                        <a href="#" className="hover:underline mr-4 md:mr-6">Licensing</a>
+                        <Link to="/estimate" className="hover:underline mr-4 md:mr-6">Estimate</Link>
                     </li>
-                    <li>
-                        <a href="#" className="hover:underline">Contact</a>
-                    </li>
+
                 </ul>
             </div>
         </footer>
